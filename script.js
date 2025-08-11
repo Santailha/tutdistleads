5a3df851f6f67b7f194ba81a2f0fe45717107fc9
Santailha <vendas@santailha.com.br>
2025-08-11 14:30:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const elementosQuePiscam = document.querySelectorAll('.pisca-alerta');
+function piscar() {
+    elementosQuePiscam.forEach(elemento => {
+       
+        if (elemento.style.visibility === 'hidden') {
+            elemento.style.visibility = 'visible';
+        } else {
+            elemento.style.visibility = 'hidden';
+        }
+    });
+}
+setInterval(piscar, 700);
